530803dc7c2b126e4c01f474ce8bcaa775ee2f06
grootenthu <amit.patil60@yahoo.in>
2021-10-26 13:44:15 +0530

    Added logic to display port number with status & also display all http headers

--- PhotoAppAccounts/src/main/java/com/photoapp/accounts/controller/AccountsController.java
+++ PhotoAppAccounts/src/main/java/com/photoapp/accounts/controller/AccountsController.java
@@ -1,14 +1,23 @@
 package com.photoapp.accounts.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccountsController {
 	
+	@Autowired
+	private Environment env;
+	
 	@GetMapping (path = "/accounts/status")
-	public String status( ) {
-		return "Up & Running";
+	public String status(@RequestHeader HttpHeaders httpHeaders) {
+		System.out.println("Headers : " + httpHeaders.toString() );
+		
+		return "Up & Running on port - " + env.getProperty("local.server.port");
 	}
 
 }
